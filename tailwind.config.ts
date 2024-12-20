a7e23c5cf60d3ca281b9f2ca43d782b6a7d5d9f5
Jacob Harvey <jacoblharvey@outlook.com>
2024-12-20 11:52:43 -0500

    Add background SVG

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        "leaves": "url('/leaves.svg')",
+      },
     },
   },
   plugins: [],
